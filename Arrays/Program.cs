e28550466612b792c7b3050c43f570785c8b727e
Matheus Alves <55101912875@info.local>
2024-10-24 16:42:34 -0300

    back-array

---
+++ Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿ 
+
+string[] carros = new string[3];
+
+Console.WriteLine($"Digite o nome do carro a ser cadastrado:");
+carro[0] = Console.ReadLine()!;
+
+for (int i = 0; i < carros.Length; i++)
+{
+    Console.WriteLine($"Digite o nome do carro a ser cadastrado:");
+    carro[i] = Console.ReadLine()!;
+
+}
+
+Console.WriteLine($"Digite o nome do carro a ser cadastrado:");
+carro[2] = Console.ReadLine()!;
+
+Console.WriteLine($"carro cadastrado é {carros[0]}, {carros[1]}, {carros[2]}");
+
+
+
+
+
---
+++ Desafio01/Program.cs
@@ -0,0 +1,1 @@
+﻿// [ ] DesafioArray01: Faça um programa que carregue 1 array tamanho 6 com números inteiros. Calcule e imprima a quantidade de números ímpares e a quantidade de números pares.
